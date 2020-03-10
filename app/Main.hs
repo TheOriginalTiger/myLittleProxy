f37883ecb29b5e21553fb0cbafe173cf9a0460ad
vladimir <vvp.vovarulit@gmail.com>
2020-03-10 14:24:56 +0700

    wow, multithreading was easy enough

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@ import ErrorCodes
 import Utils
 import Network.Stream
 import Control.Exception
+import Control.Concurrent
 
 
 main = do
@@ -25,21 +26,26 @@ handleSocket :: Socket -> IO()
 handleSocket lsock = forever $ do
                         (csock, _) <- accept lsock
                         hs <- socketConnection "" 3000 csock
-                        req <- receiveHTTP hs
-                        case req of
-                          Left _ -> handleBadRequests (5,0,0) hs
-                          Right r ->  do
-                                        putStrLn "got it !"
-                                        let newr = replaceHeader HdrUserAgent "User-Agent: ti/Pidor 3.22" r
-                                        let url = findHeader HdrHost newr
-                                        addr <- urlToDNS url 
-                                        eresp <- forwardMessage addr newr
-                                        case eresp of 
-                                          Left _ -> handleBadRequests (5,0,0) hs
-                                          Right resp -> respondHTTP hs resp
+                        forkIO $ tmp hs 
 
-                        Network.HTTP.close hs
-                    
+
+tmp :: HandleStream [Char] -> IO()
+tmp hs = do
+          req <- receiveHTTP hs --spawn
+          case req of
+            Left _ -> handleBadRequests (5,0,0) hs
+            Right r ->  do
+                          putStrLn "got it !"
+                          let newr = replaceHeader HdrUserAgent "User-Agent: ti/Pidor 3.28" r
+                          let url = findHeader HdrHost newr
+                          addr <- urlToDNS url 
+                          eresp <- forwardMessage addr newr
+                          case eresp of 
+                            Left _ -> handleBadRequests (5,0,0) hs
+                            Right resp -> respondHTTP hs resp
+          Network.HTTP.close hs
+
+
 forwardMessage :: Either DNSError [IPv4] -> Request [Char] -> IO (Result (Response [Char]))
 forwardMessage (Left _) _ = return $ Right $ formBResponse (5,0,0)
 forwardMessage (Right dnsAdress) newr = bracket cnct dcnct snd where
